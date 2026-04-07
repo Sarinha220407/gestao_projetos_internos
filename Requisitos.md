5a270731f53c41aeb1c3c6f2da137f9bcb504b56
Sarinha220407 <ext.sara.mendes@eldoradobrasil.com.br>
2026-04-07 14:26:31 -0400

    camada silver finalizada

---
+++ Requisitos.md
@@ -0,0 +1,9 @@
+# **Requisitos Gestão de Projetos Internos**
+
+*	Parâmetro de data;
+*	Serão considerados projetos os itens de trabalho que tiverem mais de 40 horas;
+*	Associar as tasks às suas devidas user stories;
+*	Considerar o backlog corretamente;
+*	Considerar as datas entre a data de criação até a data de fechamento como “Em progresso” e, se possível, criar uma condição que traga o histórico dos status; 
+*	Ao selecionar a data, o que vai aparecer? Os projetos que foram abertos naquele mês, os que estão em progresso, ou os que foram fechados; (Irá aparecer todos aqueles que foram criados e que estão em andamento no mês selecionado)
+*	Definir uma regra para projetos que tem data de criação e data de fechamento, porém não tem data de ativação. (identificar se o activated date é nulo e se closed date não é nulo, se as afirmativas anteriores forem verdadeiras, duplicar a data de criação para a activated date)
--- camada_silver.sql
+++ camada_silver.sql
@@ -1,31 +1,69 @@
-bz_projetos_pa_f:
+bz_projetos_pa_raw:
 Load
 *
 FROM [$(bronze_layer)bz_projetos_pa_f.QVD] (qvd);
+
+
 sv_projetos_pa:
+
 LOAD
+*,
+
+// Separa Data e Hora de Criação
+Date(created_date) 																									as data_criacao,  
+Time(created_date) 																									as hora_criacao,
+
+// Regra: se activated_date estiver nula ou vazia e closed_date estiver preenchida, atribuir created_date à activated_date, mantendo o formato de data e hora.
+Date(
+	IF(
+    	IsNull(activated_date) and Not IsNull (closed_date),
+        created_date,
+        activated_date))																								as data_status_active,
+
+Time(
+	IF(
+    	IsNull(activated_date) and Not IsNull (closed_date),
+        created_date,
+        activated_date))																								as hora_status_active, 
+        
+Date(closed_date)																										as data_status_closed,
+Time(closed_date)																										as hora_status_closed;
+
+
+// Load Precedente(carrega primeiro a tabela debaixo e depois a de cima)
+Load
+
+// Transformação de Dados
 num(ID) 																												as id,
 "Work Item Type" 																										as tipo,
 Title 																													as titulo,
 SubField("Assigned To", '<' ,1 ) 																						as responsavel,
 State 																													as status,
 "Tags" 																													as tags,
-Date("Created Date") 																									as data_criacao, // nao ha necessidade da formatacao 'DD/MM/YYYY' e 'hh:mm' pois o dado ja vem formatado 
-Time("Created Date") 																									as hora_criacao,
-Date(if(isnull("Activated Date"), "Created Date", "Activated Date" ))				 									as data_status_active,
-Time("Activated Date")												 													as hora_status_active, 
-Date("Closed Date")																										as data_status_closed,
-Time("Closed Date")																										as hora_status_closed,
+
+// Limpa os campos que trazem '' e os transformam em NULL
+IF(Len(Trim("Activated Date")) = 0, Null(), "Activated Date")															as activated_date,
+IF(Len(Trim("Closed Date")) = 0, Null(), "Closed Date")																	as closed_date,
+IF(Len(Trim("Created Date")) = 0, Null(), "Created Date")																as created_date,
+
+// Tranformação de Métricas
 num("Original Estimate") 																								as horas_estimadas,
 num("Remaining Work") 																									as horas_restantes,
 num("Completed Work") 																									as horas_concluidas,
+
+// Traz 'Backlog' para os campos que forem PEOPLE ANALYTICS e traz apenas os números de PEOPLE ANALYTICS\Sprint...
 if ("Iteration Path" = 'PEOPLE ANALYTICS', 'Backlog', Num(PurgeChar("Iteration Path", 'PEOPLE ANALYTICS\Sprint '))) 	as sprint,
-//SubField("Iteration Path" , '\' ,2 )
+
+// Transformação de Dados
 Priority 																												as prioridade,
 Parent 																													as parent
-RESIDENT bz_projetos_pa_f;
 
+RESIDENT bz_projetos_pa_raw;
 Store sv_projetos_pa into [$(silver_layer) sv_projetos_pa.QVD] (qvd);
+
+
+
+//Criação de Calendário
 Let vDataInicio = '2003-01-01';
 Let vDataFim = Date(MakeDate(Year(Today())+1, 12, 31), 'YYYY-MM-DD');
 sv_calendario_d:
@@ -68,10 +106,24 @@ Load
 AutoGenerate 1
 While IterNo() <= (Date('$(vDataFim)') - Date('$(vDataInicio)') + 1);
 
+
+// Filtra por Task e faz a contagem das horas, agrupando-as ao seu parent(User Story ou Enhancement)
+total_horas_us:
+LEFT JOIN (sv_projetos_pa)
+Load
+    parent 																												as id,
+    Sum(horas_estimadas) 																								as total_horas_estimadas,
+    Sum(horas_concluidas)  																								as total_horas_concluidas,
+    Sum(horas_restantes)																								as total_horas_restantes
+RESIDENT sv_projetos_pa
+WHERE tipo = 'Task'
+GROUP BY parent
+;
+
 Positions_Monthly:
 LOAD
 *,
-// STATUS baseado em intervalo real de datas
+// Define status por mês com base nas datas 
 IF(
     NOT IsNull(data_status_closed) 
     AND MonthStart(ReferenceDate) = MonthStart(data_status_closed),
@@ -90,15 +142,19 @@ IF(
 ;
 LOAD
     *,
-    // cálculo correto de meses (multi-ano)
+    // cálculo quantidade de meses entre data criação e data fechamento (ou hoje)
     (
         Year(Alt(data_status_closed, Today())) * 12 + Month(Alt(data_status_closed, Today()))
     ) -
     (
         Year(data_criacao) * 12 + Month(data_criacao)
     ) 																													as monthcount,
+    
+    // gera cada mês do intervalo
     MonthEnd(AddMonths(data_criacao, IterNo()-1)) 																		as ReferenceDate
 RESIDENT sv_projetos_pa
+
+// Controla quantas linhas serão geradas
 WHILE 
     IterNo()-1 <= 
     (
@@ -106,30 +162,20 @@ WHILE
         (Year(data_criacao) * 12 + Month(data_criacao))
     )
     AND IterNo() <= 150;
-
-// agrupa e faz a conta apenas do que sao tasks e retorna o total de horas associando o parent ao id
-total_horas_us:
-LEFT JOIN (sv_projetos_pa)
-Load
-    parent 																												as id,
-    Sum(horas_estimadas) 																								as total_horas_estimadas,
-    Sum(horas_concluidas)  																								as total_horas_concluidas,
-    Sum(horas_restantes)																								as total_horas_restantes
-RESIDENT sv_projetos_pa
-WHERE tipo = 'Task'
-GROUP BY parent
-;
-// faz a flag projeto, retorna apenas as User Storty e Enhancement com mais de 40 horas - FUNCIONANDO
+ 
 flag_projeto:
 Load
 	*,
+    
+    // Considera como projetos só as User Story e Enhancement com mais de 40 horas estimadas
     IF((tipo = 'Enhancement' or tipo = 'User Story') and total_horas_estimadas >= 40, 'Sim', 'Não')						as flag_projeto
-    RESIDENT sv_projetos_pa;
+    RESIDENT Positions_Monthly;
+ 
 
 
-
-// DROP TABLE total_horas_us
+// Limpeza de Tabelas
+DROP TABLE flag_projeto;
 DROP TABLE sv_projetos_pa;
-DROP TABLE bz_projetos_pa_f;
+DROP TABLE bz_projetos_pa_raw;
 DROP TABLE Positions_Monthly;
 DROP TABLE sv_calendario_d;
